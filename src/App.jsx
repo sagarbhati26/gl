197ae6ae2b8dffad630cd52182a1069c6988aa1a
sagarbhati26 <sagarbhati2616@gmail.com>
2023-05-20 12:33:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { GoogleLogin } from '@react-oauth/google';
+const App = () => {
+
+return (
+    <div>
+        <h2>React Google Login</h2>
+        <br />
+        <br />
+        <GoogleLogin  />
+    </div>
+)
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App';
+
+ReactDOM.render(
+    
+<GoogleOAuthProvider clientId='765046310419-vp2r97ees837qloag8bal447v98eparl.apps.googleusercontent.com'>
+
+
+<React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </GoogleOAuthProvider>,
+    
+    
+    document.getElementById("root"))
+
+
+
+
+
+    // 765046310419-vp2r97ees837qloag8bal447v98eparl.apps.googleusercontent.com
+    
+    // 765046310419-vp2r97ees837qloag8bal447v98eparl.apps.googleusercontent.com
